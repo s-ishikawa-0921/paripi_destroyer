1205059a2deb5cf2e44de90f77790f6f561af6a6
satoshi.ishikawa <satoshi.ishikawa@careritz.co.jp>
2019-01-15 20:01:44 +0900

    debug設定変更

--- webapi.py
+++ webapi.py
@@ -100,4 +100,4 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=False, host='0.0.0.0', port=80)
